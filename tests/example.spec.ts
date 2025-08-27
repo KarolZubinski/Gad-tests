025da99235a327fa1da4f2006952f1f61a21fb80
KarolZubinski <zubinski.k@gmail.com>
2025-08-27 21:12:31 +0200

    add new articles page test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,20 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('home page title', async ({ page }) => {
+  //Act
   await page.goto('http://localhost:3000/');
+
+  //Assert
   await page.waitForLoadState();
   await expect(page).toHaveTitle(/GAD/);
 });
+
+test('articles page title', async ({ page }) => {
+  //Act
+  await page.goto('http://localhost:3000/articles.html');
+
+  //Assert
+  await page.waitForLoadState();
+  await expect(page).toHaveTitle(/GAD/);
+});
+
