b360b91522ce354f112f02792110ff01d3ef4da6
KarolZubinski <zubinski.k@gmail.com>
2025-09-23 08:07:01 +0200

    test:updated menu.spec.ts

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,6 +1,6 @@
+import { MainMenuComponent } from '../../src/components/main.menu.component';
 import { ArticlesPage } from '../../src/pages/articles.page';
 import { CommentsPage } from '../../src/pages/comments.page';
-import { HomePage } from '../../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify menu main buttons', () => {
@@ -14,7 +14,27 @@ test.describe('Verify menu main buttons', () => {
 
       //Act
       await articlesPage.goto();
-      await page.getByTestId('open-comments').click();
+      const menuComponent = new MainMenuComponent(page);
+      await menuComponent.commentsButton.click();
+
+      //Assert
+      const title = await commentsPage.title();
+      expect(title).toContain('Comments');
+    },
+  );
+
+  test(
+    'articles button navigates to comments page',
+    { tag: '@GAD-R01-03' },
+    async ({ page }) => {
+      //Arrange
+      const articlesPage = new ArticlesPage(page);
+      const commentsPage = new CommentsPage(page);
+
+      //Act
+      await articlesPage.goto();
+      const menuComponent = new MainMenuComponent(page);
+      await menuComponent.commentsButton.click();
 
       //Assert
       const title = await commentsPage.title();
