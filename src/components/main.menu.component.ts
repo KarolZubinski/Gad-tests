62e33981e03db6f3e3e8b8b38a297d341573cfe5
KarolZubinski <zubinski.k@gmail.com>
2025-09-29 19:33:07 +0200

    test: added new locator

--- src/components/main.menu.component.ts
+++ src/components/main.menu.component.ts
@@ -1,10 +1,13 @@
 import { Locator, Page } from '@playwright/test';
+import { name } from 'eslint-plugin-prettier/recommended';
 
 export class MainMenuComponent {
   commentsButton: Locator;
   articlesButton: Locator;
+  homePage: Locator;
   constructor(private page: Page) {
     this.commentsButton = this.page.getByTestId('open-comments');
     this.articlesButton = this.page.getByTestId('open-articles');
+    this.homePage = this.page.getByTestId('link', { name: '🦎 GAD' });
   }
 }
