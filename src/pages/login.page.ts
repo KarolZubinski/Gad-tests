2a69593697486b7419fee569071829205c37b876
KarolZubinski <zubinski.k@gmail.com>
2025-10-06 08:22:13 +0200

    test:added login page

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,11 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { BasePage } from './base.page';
+import { Page } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/login/');
-  await page.getByRole('textbox', { name: 'Enter User Email' }).click();
-  await page.getByRole('textbox', { name: 'Enter User Email' }).fill('Moses.Armstrong@Feest.ca');
-  await page.getByRole('textbox', { name: 'Enter Password' }).click();
-  await page.getByRole('textbox', { name: 'Enter Password' }).fill('test1');
-  await page.getByRole('button', { name: 'LogIn' }).click();
-  await page.getByTestId('hello').click();
-});
+export class HomePage extends BasePage {
+  url = '/login/';
+  constructor(page: Page) {
+    super(page);
+  }
+}
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,9 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { HomePage } from '../src/pages/home.page';
+import { test } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('login page', async ({ page }) => {
+  //Arrange
+  const loginPage = new HomePage(page);
+  await loginPage.goto();
+  //Act
+
+  //Assert
   await page.goto('http://localhost:3000/login/');
   await page.getByRole('textbox', { name: 'Enter User Email' }).click();
-  await page.getByRole('textbox', { name: 'Enter User Email' }).fill('Moses.Armstrong@Feest.ca');
+  await page
+    .getByRole('textbox', { name: 'Enter User Email' })
+    .fill('Moses.Armstrong@Feest.ca');
   await page.getByRole('textbox', { name: 'Enter Password' }).click();
   await page.getByRole('textbox', { name: 'Enter Password' }).fill('test1');
   await page.getByRole('button', { name: 'LogIn' }).click();
