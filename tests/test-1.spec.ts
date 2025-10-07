acb8a8e3b5e571f030a99801278acc473a8f91a0
KarolZubinski <zubinski.k@gmail.com>
2025-10-07 08:22:29 +0200

    test:added new method login

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,20 +1,14 @@
-import { HomePage } from '../src/pages/home.page';
+import { LoginPage } from '../src/pages/login.page';
 import { test } from '@playwright/test';
 
 test('login page', async ({ page }) => {
   //Arrange
-  const loginPage = new HomePage(page);
+  const userEmail = 'Moses.Armstrong@Feest.ca';
+  const userPassword = 'test1';
+  const loginPage = new LoginPage(page);
   await loginPage.goto();
   //Act
 
   //Assert
-  await page.goto('http://localhost:3000/login/');
-  await page.getByRole('textbox', { name: 'Enter User Email' }).click();
-  await page
-    .getByRole('textbox', { name: 'Enter User Email' })
-    .fill('Moses.Armstrong@Feest.ca');
-  await page.getByRole('textbox', { name: 'Enter Password' }).click();
-  await page.getByRole('textbox', { name: 'Enter Password' }).fill('test1');
-  await page.getByRole('button', { name: 'LogIn' }).click();
-  await page.getByTestId('hello').click();
+  await loginPage.login(userEmail, userPassword);
 });
