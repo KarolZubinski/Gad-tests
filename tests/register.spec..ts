9c81c294b0c5f74e3eca07c0265e6c66954b6ea8
KarolZubinski <zubinski.k@gmail.com>
2025-10-30 08:09:37 +0100

    test: added register test

---
+++ tests/register.spec..ts
@@ -0,0 +1,21 @@
+import { LoginPage } from '../src/pages/login.page';
+import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify register', () => {
+  test('register with correct data @GAD_R03_01', async ({ page }) => {
+    await page.goto('http://localhost:3000/register.html');
+    await page.getByTestId('firstname-input').click();
+    await page.getByTestId('firstname-input').fill('Janina');
+    await page.getByTestId('firstname-input').press('Tab');
+
+    await page.getByTestId('lastname-input').fill('Nowak');
+    await page.getByTestId('firstname-input').press('Tab');
+    await page.getByTestId('email-input').fill('testowy@test.test');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('test123');
+    await page.getByTestId('register-button').click();
+    await page.getByTestId('alert-popup').click();
+  });
+});
