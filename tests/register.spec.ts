fe2e2b7fbf800139ff028d2be642415f84f82774
KarolZubinski <zubinski.k@gmail.com>
2025-11-03 09:07:04 +0100

    test: added register page.ts

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -5,17 +5,15 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
   test('register with correct data @GAD_R03_01', async ({ page }) => {
+    //Arrange
     await page.goto('http://localhost:3000/register.html');
-    await page.getByTestId('firstname-input').click();
     await page.getByTestId('firstname-input').fill('Janina');
-    await page.getByTestId('firstname-input').press('Tab');
-
     await page.getByTestId('lastname-input').fill('Nowak');
-    await page.getByTestId('firstname-input').press('Tab');
-    await page.getByTestId('email-input').fill('testowy@test.test');
-    await page.getByTestId('password-input').click();
+    await page
+      .getByTestId('email-input')
+      .fill(`testowy${Date.now()}@test.test`);
+
     await page.getByTestId('password-input').fill('test123');
     await page.getByTestId('register-button').click();
-    await page.getByTestId('alert-popup').click();
   });
 });
