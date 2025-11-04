ad6e0aa6bc96b54acff4fe1071ed27a0fc2a4c9b
KarolZubinski <zubinski.k@gmail.com>
2025-11-04 20:27:28 +0100

    test: added consts to register.spec.ts

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,19 +1,19 @@
-import { LoginPage } from '../src/pages/login.page';
-import { WelcomePage } from '../src/pages/welcome.page';
-import { testUser1 } from '../src/test-data/user.data';
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test.describe('Verify register', () => {
   test('register with correct data @GAD_R03_01', async ({ page }) => {
     //Arrange
+    const userFirstName = 'Janina';
+    const userLastName = 'Nowak';
+    const userEmail = `testowy${Date.now()}@test.test`;
+    const userPassword = 'test123';
+    
     await page.goto('http://localhost:3000/register.html');
-    await page.getByTestId('firstname-input').fill('Janina');
-    await page.getByTestId('lastname-input').fill('Nowak');
-    await page
-      .getByTestId('email-input')
-      .fill(`testowy${Date.now()}@test.test`);
+    await page.getByTestId('firstname-input').fill(userFirstName);
+    await page.getByTestId('lastname-input').fill(userLastName);
+    await page.getByTestId('email-input').fill(userEmail);
 
-    await page.getByTestId('password-input').fill('test123');
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('register-button').click();
   });
 });
