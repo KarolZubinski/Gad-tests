87bd65ae82e6b4bf7d531a6817ee16a30172394a
KarolZubinski <zubinski.k@gmail.com>
2025-11-21 08:15:54 +0100

    test: added registerUserData

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,3 +1,5 @@
+import { register } from 'module';
+import { RegisterUser } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
@@ -9,13 +11,25 @@ test.describe('Verify register', () => {
     page,
   }) => {
     //Arrange
-    const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
-    const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
-    const userEmail = faker.internet.email({
-      firstName: userFirstName,
-      lastName: userLastName,
+    // const userFirstName = faker.person.firstName().replace(/[^A-Za-z]/g, '');
+    // const userLastName = faker.person.lastName().replace(/[^A-Za-z]/g, '');
+    // const userEmail = faker.internet.email({
+    //   firstName: userFirstName,
+    // //   lastName: userLastName,
+    // });
+    // const userPassword = faker.internet.password();
+
+    const registerUserData: RegisterUser = {
+      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+      userEmail: '',
+      userPassword: faker.internet.password(),
+    };
+
+    registerUserData.userEmail = faker.internet.email({
+      firstName: registerUserData.userFirstName,
+      lastName: registerUserData.userLastName,
     });
-    const userPassword = faker.internet.password();
 
     const registerPage = new RegisterPage(page);
 
